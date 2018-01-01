d74ea4ae10af6b42991764c0e4b58af82034d6dc
Motaz AlShishani <motaz_shishani@hotmail.com>
2018-01-01 03:13:25 +0200

    Create project

---
+++ SendGridIntegration/SendGridIntegration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendGridIntegration.Startup))]
+namespace SendGridIntegration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
